9508bcc2fb14f8334f909f938a4f937267b1e6d7
syufi <syufiz4@gmail.com>
2017-02-25 08:44:20 +0700

    upload

---
+++ resources/views/index.blade.php
@@ -0,0 +1,380 @@
+<html>
+  <head>
+  <title>COMSSCOPE.com</title>
+  	<style type="text/css">
+       body{
+       	background: #333333;
+       	margin: 0px;
+       }
+
+       #wrapper{
+       	background: white;
+       	width: 960px;
+       	margin: 0 auto;
+       	padding:15px 10px 0px;
+       }
+
+       #headerImage{
+       	/*background: url("headerImage.jpg");*/
+       }
+
+       #menu{
+       	background: #333333;
+       	padding: 8px;
+        text-align: center;
+       }
+
+       #menu a{
+       	font-family: arial;
+       	font-weight: bold;
+       	text-transform: uppercase;
+       	font-size: 10px;
+        text-decoration: none;
+        color:white;
+        border-left: 1px dotted white;
+        padding-left: 10px;
+        margin-right: 10px;
+       }
+
+       #menu a:hover{
+       	color:#cb3000;
+       }
+
+       #content{
+       	border: 1px dashed #cecec2;
+
+       }
+
+       #sidebar{
+       	float: left;
+       	width: 230px;
+       	border: 1px dashed #cecec2;
+       	padding: 20px;
+       }
+
+       #sidebar h3{
+       	margin-bottom: 5px;
+       	text-transform: uppercase;
+       	font-size: 14px;
+       	font-family: arial;
+       }
+
+       #sidebar ul{
+       	margin: 0px;
+       	padding: 0px;
+       }
+
+       #sidebar ul li{
+        list-style-type: none;
+        border-bottom: 1px dotted #cecec2;
+        padding: 3px 0;
+       }
+
+       #sidebar ul li a{
+        text-decoration: none;
+        text-transform: uppercase;
+        font-size: 12px;
+        color: #5b5b63;
+        font-family: arial;
+       }
+
+       #sidebar ul li a:hover{
+       	color: #cb3000;
+       }
+
+       #sidebar ul li:first-child{
+        border-top: 1px dotted #cecec2;
+       }
+
+       #sidebar ul li:last-child{
+       	margin-bottom: 30px;
+       }
+
+       #sidebar .lastKomen li a{
+       	text-transform: none;
+       }
+
+       #mainContent{
+       	float:left;
+       	width: 675px;
+       	padding: 5px;
+        border-right: 1px dashed #cecec2;
+       }
+
+       /*#mainContent ul{
+        padding:0px;
+        margin-left: 15px;
+       }*/
+
+       #mainContent .postGrid{
+         list-style-type: none;
+         display: block;
+         width: 300px;
+         height: 510px;
+         float: left;
+         /*border-bottom: 1px dashed #cecec2;*/
+         padding:5px 15px;
+         color: #363231;
+         overflow: hidden;
+
+        color:#5C5054;
+       }
+
+       #mainContent .postGrid a{
+        text-decoration: none;
+        color: #cb3000;
+       }
+
+
+     
+       #mainContent .postGrid .tanggal .judul{
+        float: left;
+        margin-left: 5px;
+        font-family: arial;
+        color: grey;
+       }
+	   
+       #mainContent .postGrid .komen{
+       }
+
+       #mainContent .last{
+       	border-left: 1px dashed #cecec2;
+       }
+
+       #mainContent .separator{
+        background: url("dot.png") 0 100% repeat-x;
+        height: 30px;
+        margin-bottom: 30px;
+        clear: both;
+       }
+
+       #mainContent .thumb{
+        padding: 5px;
+        border: 1px dashed #cecec2;
+        margin-top: 15px;
+       }
+
+       #mainContent h3{
+        font-family: 'Merienda', cursive;
+        color: #2c353e;
+       }
+
+       #mainContent #pagination{
+        margin-left: 30px;
+       }
+
+       #mainContent #pagination a{
+        text-decoration: none;
+        color:#cb3000;
+        font-family: arial;
+        font-weight: bold;
+        border-right: 1px dotted #cecec2;
+        margin-right: 10px;
+        padding-right: 10px;
+       }
+	   
+
+       #footer{
+        clear: both;
+        background: url("footer.png");
+        height: 80px;
+        padding: 10px;
+        padding-top: 20px;
+       }
+
+       #footer a{
+        text-decoration: none;
+        color:#cb3000;
+       }
+
+       #footer .sumber{
+        float: right;
+       }
+
+.footer-wrapper{background:#043e7f; overflow:hidden; padding-bottom:20px;font-family: 'Merienda', cursive;}
+.footer{ width:960px; margin:0 auto; overflow:hidden; margin-top:60px;font-family: 'Merienda', cursive;}
+.footer .panel{ float:left; width:300px;font-family: 'Merienda', cursive;}
+.footer .panel .title h1{ margin-bottom:30px;font-family: 'Merienda', cursive;}
+.footer .panel .contnet{margin-top:20px;font-family: 'Merienda', cursive;}
+.footer .panel .contnet ul li{list-style:none;  border-bottom:#104f94 solid 1px;font-family: 'Merienda', cursive;}
+.footer .panel .contnet ul li a{ color:#85aedb; text-decoration:none; line-height:40px; font-size:14px;font-family: 'Merienda', cursive;}
+.footer .panel .contnet ul li a:hover{color:#ffc01d;font-family: 'Merienda', cursive;}
+
+.footer .panel .contnet img{ float:left; margin-right:20px;font-family: 'Merienda', cursive;}
+.footer .panel .contnet spna{ text-decoration:underline; color:#ffc01d;font-family: 'Merienda', cursive;}
+.footer .panel .contnet p{ color:#85aedb; font-size:14px; padding-top:6px;font-family: 'Merienda', cursive;}
+.phone{ margin-top:20px; padding-bottom:30px; margin-bottom:30px; border-bottom:#104f94 solid 1px;}
+.phone img{ float:left; margin-right:10px;}
+.phone h1{ color:#ffc01d;}
+  	</style>
+  <head>
+  <body>
+     <div id="wrapper">
+
+         <div id="header">
+
+         	<div id="headerImage">
+         		<img src="header.png" />
+         	</div> <!-- akhir headerImage-->
+
+         	<div id="menu">
+         		<a href="index">HOME</a>
+         		<a href="#">PRODUCTS</a>
+         		<a href="tentang">ABOUT</a>
+         		<a href="map">MAP</a>
+         		<a href="#">HUBUNGI KAMI</a>
+         	</div>
+
+         </div> <!-- akhir header -->
+
+         <div id="content">
+
+         	<div id="sidebar">
+         		<h3>Social Media</h3>
+         	    <ul>
+                   <li><a href="#">Instagram</a></li>
+                   <li><a href="#">Facebook</a></li
+                   <li><a href="#">Youtube</a></li>
+                   <li><a href="#">Flickr</a></li>
+                   <li><a href="#">Linkedin</a></li>
+                   <li><a href="#">Twitter</a></li>
+         	    </ul>
+
+         	    
+
+         	    <h3>News From Comscope</h3>
+         	    <ul class="lastKomen">
+                   <li>Jan 31, 2017 <a href="#"> CommScope Files Patent Infringement Suit Against Clearfield</a></li>
+                   <li>Jan 24, 2017 <a href="#">CommScope Showcases High Speed Solutions at BICSI to Meet Growing Bandwidth Demand</a></li>
+                   <li>Jan 17, 2017 <a href="#">CommScope to Release Fourth Quarter 2016 Financial Results February 23</a></li>
+                
+         	    </ul>
+
+            </div> <!-- akhir sidebar -->
+
+            <div id="mainContent">
+            	
+            		<div class="postGrid">
+                  <div class="meta">
+                    <div class="tanggal">
+                      <span class="judul"><div>Berita Terkini</div></span>
+                    </div>
+                    
+                  </div>
+
+                  
+                  <h3>Internet Ultra Cepat Download Streaming Gaming</h3>
+                
+                  Rasakan kecepatan internet hingga 300 Mbps! 
+				  Download dan Upload Stabil, Streaming lancar, 
+				  dan main game tanpa lag. </div>
+
+            		<div class="postGrid last">
+                  <div class="meta">
+                    <div class="tanggal">
+                     
+                       <span class="judul"><div>Berita Terkini</div></span>
+                    </div>
+                    
+                  </div>
+				  
+                  <h3>Hiburan Tanpa Batas di Rumah Anda</h3>
+                  
+                  Tayangan hiburan dan Informatif hadir 
+				  di rumah anda dari Channel International 
+				  dan lokal. Kami memberikan hiburan lainnya 
+				  melalui Entertainemnt Box yang dapat merubah
+				  TV Biasa menjadi Smart TV. </div>
+                
+                <div class="separator"></div>
+
+            		<div class="postGrid">
+                  <div class="meta">
+                    <div class="tanggal">
+               
+                      <span class="judul"><div>Berita Terkini</div></span>
+                    </div>
+                    
+                  </div>
+
+                  
+                  <h3>Harga Terjangkau Untuk Kecepatan Ultra Cepat</h3>
+                  
+                 Hanya Commscope yang memberikan kamu koneksi internet 
+				 lebih cepat dan unlimited, tanpa membayar lebih. 
+				 Harga lebih murah dengan kecepatan yang lebih 
+				 cepat sesuai pilihan Anda, dari mulai 50 Mbps, 
+				 100 Mbps, 150Mbps dan 300 Mbps.</div>
+
+            		<div class="postGrid last">
+                  <div class="meta">
+                    <div class="tanggal">
+                      
+                      <span class="judul"><div>Berita Terkini</div></span>
+                    </div>
+                    
+                  </div>
+				  
+                  <h3>Cek Area Jangkauan</h3>
+                  
+                  Penting untuk mengetahui terlebih
+				  dahulu apakah jaringan comsscope 
+				  sudah ada di rumah Anda. </div>
+
+                <div class="separator"></div>
+            </div> <!-- akhir mainContent -->
+
+         </div> <!-- akhir content -->
+
+         <div id="footer">
+          
+         </div> <!-- akhir footer -->
+  <div class="footer">
+    <div class="panel marRight30">
+      <div class="title">
+        <h3 align="center">Support Center</h3>
+      </div>
+      <div class="contnet">
+        <ul><li><a href="#">Contacs Us by email</a></li>
+          <li><a href="#">Partner Locator</a></li>
+		  <li><a href="#">Sales Locator</a></li>
+          <li><a href="#">Services</a></li>
+          <li class="border-bottom"><a href="#">Training</a></li>
+        </ul>
+      </div>
+    </div>
+	
+    <div class="panel marRight30">
+      <div class="title">
+        <h3 align="center">About Us</h3>
+      </div>
+      <div class="contnet">
+        <ul><li><a href="#">Careers</a></li>
+          <li><a href="#">Corporates Respons</a></li>
+		  <li><a href="#">Time Line</a></li>
+		  <li><a href="#">Investor Relation</a></li>
+          <li class="border-bottom"><a href="#">Management Team</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="panel marRight30">
+      <div class="title">
+        <h3 align="center">News Center</h3>
+      </div>
+      <div class="contnet">
+        <ul><li><a href="#">Blog</a></li>
+          <li><a href="#">Events</a></li>
+		  <li><a href="#">Infographics</a></li>
+		  <li><a href="#">Media Contacs</a></li>
+          <li class="border-bottom"><a href="#">Press Kit</a></li>
+        </ul>
+      </div>
+    </div>
+    
+  <div class="clear"></div>
+</div>
+     </div> <!-- akhir wrapper -->
+
+  </body>
+</html>
---
+++ resources/views/map.blade.php
@@ -0,0 +1,375 @@
+<html>
+  <head>
+  <title>CommScope.com</title>
+    <style type="text/css">
+       body{
+        background: #333333;
+        margin: 0px;
+       }
+
+       #wrapper{
+        background: white;
+        width: 960px;
+        margin: 0 auto;
+        padding:15px 10px 0px;
+       }
+
+       #headerImage{
+        /*background: url("headerImage.jpg");*/
+       }
+
+       #menu{
+        background: #333333;
+        padding: 8px;
+        text-align: center;
+       }
+
+       #menu a{
+        font-family: arial;
+        font-weight: bold;
+        text-transform: uppercase;
+        font-size: 10px;
+        text-decoration: none;
+        color:white;
+        border-left: 1px dotted white;
+        padding-left: 10px;
+        margin-right: 10px;
+       }
+
+       #menu a:hover{
+        color:#cb3000;
+       }
+
+       #content{
+        border: 1px dashed #cecec2;
+
+       }
+
+       #sidebar{
+        float: left;
+        width: 230px;
+        border: 1px dashed #cecec2;
+        padding: 20px;
+       }
+
+       #sidebar h3{
+        margin-bottom: 5px;
+        text-transform: uppercase;
+        font-size: 14px;
+        font-family: arial;
+       }
+
+       #sidebar ul{
+        margin: 0px;
+        padding: 0px;
+       }
+
+       #sidebar ul li{
+        list-style-type: none;
+        border-bottom: 1px dotted #cecec2;
+        padding: 3px 0;
+       }
+
+       #sidebar ul li a{
+        text-decoration: none;
+        text-transform: uppercase;
+        font-size: 12px;
+        color: #5b5b63;
+        font-family: arial;
+       }
+
+       #sidebar ul li a:hover{
+        color: #cb3000;
+       }
+
+       #sidebar ul li:first-child{
+        border-top: 1px dotted #cecec2;
+       }
+
+       #sidebar ul li:last-child{
+        margin-bottom: 30px;
+       }
+
+       #sidebar .lastKomen li a{
+        text-transform: none;
+       }
+
+       #mainContent{
+        float:left;
+        width: 675px;
+        padding: 5px;
+        border-right: 1px dashed #cecec2;
+       }
+
+       /*#mainContent ul{
+        padding:0px;
+        margin-left: 15px;
+       }*/
+
+       #mainContent .postGrid{
+         list-style-type: none;
+         display: block;
+         width: 300px;
+         height: 510px;
+         float: left;
+         /*border-bottom: 1px dashed #cecec2;*/
+         padding:5px 15px;
+         color: #363231;
+         overflow: hidden;
+
+        color:#5C5054;
+       }
+
+       #mainContent .postGrid a{
+        text-decoration: none;
+        color: #cb3000;
+       }
+
+
+     
+       #mainContent .postGrid .tanggal .bulan{
+        float: left;
+        margin-left: 5px;
+        font-family: arial;
+        color: grey;
+       }
+     
+       #mainContent .postGrid .komen{
+       }
+
+       #mainContent .last{
+        border-left: 1px dashed #cecec2;
+       }
+
+       #mainContent .separator{
+        background: url("dot.png") 0 100% repeat-x;
+        height: 30px;
+        margin-bottom: 30px;
+        clear: both;
+       }
+
+       #mainContent .thumb{
+        padding: 5px;
+        border: 1px dashed #cecec2;
+        margin-top: 15px;
+       }
+
+       #mainContent h3{
+        font-family: 'Merienda', cursive;
+        color: #2c353e;
+       }
+
+       #mainContent #pagination{
+        margin-left: 30px;
+       }
+
+       #mainContent #pagination a{
+        text-decoration: none;
+        color:#cb3000;
+        font-family: arial;
+        font-weight: bold;
+        border-right: 1px dotted #cecec2;
+        margin-right: 10px;
+        padding-right: 10px;
+       }
+     
+
+       #footer{
+        clear: both;
+        background: url("footer.png");
+        height: 80px;
+        padding: 10px;
+        padding-top: 20px;
+       }
+
+       #footer a{
+        text-decoration: none;
+        color:#cb3000;
+       }
+
+       #footer .sumber{
+        float: right;
+       }
+.footer-wrapper{background:#043e7f; overflow:hidden; padding-bottom:20px;font-family: 'Merienda', cursive;}
+.footer{ width:960px; margin:0 auto; overflow:hidden; margin-top:60px;font-family: 'Merienda', cursive;}
+.footer .panel{ float:left; width:300px;font-family: 'Merienda', cursive;}
+.footer .panel .title h1{ margin-bottom:30px;font-family: 'Merienda', cursive;}
+.footer .panel .contnet{margin-top:20px;font-family: 'Merienda', cursive;}
+.footer .panel .contnet ul li{list-style:none;  border-bottom:#104f94 solid 1px;font-family: 'Merienda', cursive;}
+.footer .panel .contnet ul li a{ color:#85aedb; text-decoration:none; line-height:40px; font-size:14px;font-family: 'Merienda', cursive;}
+.footer .panel .contnet ul li a:hover{color:#ffc01d;font-family: 'Merienda', cursive;}
+
+.footer .panel .contnet img{ float:left; margin-right:20px;font-family: 'Merienda', cursive;}
+.footer .panel .contnet spna{ text-decoration:underline; color:#ffc01d;font-family: 'Merienda', cursive;}
+.footer .panel .contnet p{ color:#85aedb; font-size:14px; padding-top:6px;font-family: 'Merienda', cursive;}
+.phone{ margin-top:20px; padding-bottom:30px; margin-bottom:30px; border-bottom:#104f94 solid 1px;}
+.phone img{ float:left; margin-right:10px;}
+.phone h1{ color:#ffc01d;}
+    </style>
+  <head>
+  <body>
+     <div id="wrapper">
+
+         <div id="header">
+
+          <div id="headerImage">
+            <img src="header.png" />
+          </div> <!-- akhir headerImage-->
+
+          <div id="menu">
+            <a href="index">HOME</a>
+            <a href="#">PRODUCTS</a>
+            <a href="tentang">ABOUT</a>
+            <a href="map">MAP</a>
+            <a href="#">HUBUNGI KAMI</a>
+          </div>
+
+         </div> <!-- akhir header -->
+
+         <div id="content">
+
+          <div id="sidebar">
+            <h3>Social Media</h3>
+              <ul>
+                   <li><a href="#">Instagram</a></li>
+                   <li><a href="#">Facebook</a></li
+                   <li><a href="#">Youtube</a></li>
+                   <li><a href="#">Flickr</a></li>
+                   <li><a href="#">Linkedin</a></li>
+                   <li><a href="#">Twitter</a></li>
+              </ul>
+
+              
+
+              <h3>News From Comscope</h3>
+              <ul class="lastKomen">
+                   <li>Jan 31, 2017 <a href="#"> CommScope Files Patent Infringement Suit Against Clearfield</a></li>
+                   <li>Jan 24, 2017 <a href="#">CommScope Showcases High Speed Solutions at BICSI to Meet Growing Bandwidth Demand</a></li>
+                   <li>Jan 17, 2017 <a href="#">CommScope to Release Fourth Quarter 2016 Financial Results February 23</a></li>
+                
+              </ul>
+
+            </div> <!-- akhir sidebar -->
+
+            <div id="mainContent">
+              
+                
+                 
+                  <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+ 
+<style>
+          html, body {
+               font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;
+               font-size: medium;
+               }
+          #map {
+               width: 500px;
+               height: 500px;
+               border: 1px solid black;
+          }
+</style>
+ 
+<script type="text/javascript">
+          (function() {
+               window.onload = function() {
+ 
+               // Membuat konfigurasi umum peta berbasis Google Maps
+               // zoom: untuk perbesaran/skala peta;
+               // center: untuk menentukan titik koordinat tengah peta;
+               // mapTypeId: untuk menentukan tipe peta yang digunakan;
+               var options = {
+                    zoom: 17,
+                    center: new google.maps.LatLng(-7.917357, 112.634396),
+                    mapTypeId: google.maps.MapTypeId.HYBRID 
+               };
+ 
+               // Membuat objek peta Google Maps, memanggil elemen HTML dengan id = 'map' 
+               var map = new google.maps.Map(document.getElementById('map'), options);
+ 
+               // Menambahkan marker (penanda) ke dalam peta
+               var marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(-7.917357, 112.634396),
+                    map: map,
+                    title: 'Klik Saya'
+               });
+ 
+               // Membuat InfoWindow dengan memunculkan informasi/teks ketika di-klik
+               var infowindow = new google.maps.InfoWindow({
+                    content: 'Ini adalah sebuah'
+               });
+ 
+               // Menambahkan event Click pada penanda
+               google.maps.event.addListener(marker, 'click', function() {
+ 
+               // Memanggil 'open method' InfoWindow
+               infowindow.open(map, marker);
+               });
+ 
+               };
+          })();
+</script>
+</head>
+ 
+<body>
+<center>
+ 
+
+<div id="map"></div>
+
+                <div class="separator"></div>
+            </div> <!-- akhir mainContent -->
+
+         </div> <!-- akhir content -->
+
+         <div id="footer">
+          
+         </div> <!-- akhir footer -->
+  <div class="footer">
+    <div class="panel marRight30">
+      <div class="title">
+        <h3 align="center">Support Center</h3>
+      </div>
+      <div class="contnet">
+        <ul><li><a href="#">Contacs Us by email</a></li>
+          <li><a href="#">Partner Locator</a></li>
+      <li><a href="#">Sales Locator</a></li>
+          <li><a href="#">Services</a></li>
+          <li class="border-bottom"><a href="#">Training</a></li>
+        </ul>
+      </div>
+    </div>
+  
+    <div class="panel marRight30">
+      <div class="title">
+        <h3 align="center">About Us</h3>
+      </div>
+      <div class="contnet">
+        <ul><li><a href="#">Careers</a></li>
+          <li><a href="#">Corporates Respons</a></li>
+      <li><a href="#">Time Line</a></li>
+      <li><a href="#">Investor Relation</a></li>
+          <li class="border-bottom"><a href="#">Management Team</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="panel marRight30">
+      <div class="title">
+        <h3 align="center">News Center</h3>
+      </div>
+      <div class="contnet">
+        <ul><li><a href="#">Blog</a></li>
+          <li><a href="#">Events</a></li>
+      <li><a href="#">Infographics</a></li>
+      <li><a href="#">Media Contacs</a></li>
+          <li class="border-bottom"><a href="#">Press Kit</a></li>
+        </ul>
+      </div>
+    </div>
+    
+  <div class="clear"></div>
+</div>
+     </div> <!-- akhir wrapper -->
+
+  </body>
+</html>
